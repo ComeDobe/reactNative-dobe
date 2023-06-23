79516a50a60c2cdf4b2f75eb4559916109c02527
Christian DOBE <christiandobe@hotmail.fr>
2023-06-23 14:29:36 +0200

    Initial commit

---
+++ screens/ListeDemande/ListeDemandeStyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import theme from "../../theme";
+
+export default function ListeDemandeStyles() {
+  const colors = theme();
+  const styles = StyleSheet.create({
+   
+  });
+  return styles;
+}
